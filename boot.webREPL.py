a3b8ad36ab2535dbdd2e4d69b86c2d2befdf0111
01604987 <e1604987@student.tuwien.ac.at>
2023-12-26 17:38:50 +0100

    added webrepl boot script

---
+++ boot.webREPL.py
@@ -0,0 +1,38 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import os, machine, time, network
+os.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+import webrepl
+
+
+def do_connect():
+    
+    # WiFi SSID and Password
+    wifi_ssid = None         # YOUR WiFi SSID
+    wifi_password = None     # YOUR WiFi PASSWORD
+
+    # Wireless config : Station mode
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+
+    # Continually try to connect to WiFi access point
+    while not station.isconnected():
+    
+        # Try to connect to WiFi access point
+        print("Connecting...")
+        station.connect(wifi_ssid, wifi_password)
+        time.sleep(10)
+
+    # Display connection details
+    print("Connected!")
+    print("My IP Address:", station.ifconfig()[0])
+    
+
+
+do_connect()
+webrepl.start()
+
+gc.collect()
+
