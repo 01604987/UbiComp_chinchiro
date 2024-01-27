f2c47f0c167186be10d270c98ffd7b40017618c6
01604987 <e1604987@student.tuwien.ac.at>
2024-01-27 21:35:18 +0100

    updated logic with prototype shaking impl

--- logic.py
+++ logic.py
@@ -64,8 +64,8 @@ class Logic:
         self.s_m.set_game_state("initial")
 
         self.btns.set_btn_irq("right", self._play_sound)
-        self.audio.player1.volume(15)
-        self.audio.player2.volume(15)
+        self.audio.player1.volume(20)
+        self.audio.player2.volume(20)
         #self.btns.set_btn_irq("right", self._set_light)
         self.btns.set_btn_irq("left", self._end_game)
 
@@ -92,14 +92,110 @@ class Logic:
             # self.led.set_light(self.btns.get_r_pressed(len(LIGHTS))))
             
             # poll distance sensor
-            distance = 0
+            distance = 5
             # if sensor measure > interval
             if distance > interval:
                 # stop any blinking leds, timers or sound effect that are designed only for initial
                 break
 
     def _shaking(self):
-        raise(NotImplementedError)
+        
+        from lib.mpu6050 import accel
+        from machine import I2C, Pin
+        import time
+
+        i = I2C(scl=Pin(12), sda=Pin(13), freq = 100000)
+        print(i)
+        a = accel(i)
+        mem_info()
+
+        default_interval = 10000
+
+        # [[current val, last val, interval, max val], ...]
+        values = [[0,0,default_interval, 0],[0,0,default_interval, 0],[0,0,default_interval, 0]]
+        max_set = 0
+        def acc(accel, values):
+            raw = accel.get_values()
+            axis = [0,0,0]
+            axis[0] = raw['AcX']
+            axis[1] = raw['AcY']
+            axis[2] = raw['AcZ'] - 19000
+            
+            for index, val in enumerate(values):
+                
+                #if abs(axis[index]) > 500:            
+                    # copy current value to last value
+                    val[1] = val[0]
+                    # update current value with new value
+                    val[0] = axis[index]
+                
+        def calculate_interval(axis ,values, default_interval):
+            if axis == None:
+                for val in values:
+                    val[2] = default_interval
+                    #print("resetting interval for all")
+                return
+            
+            for index, val in enumerate(values):
+                if index == axis:
+                    continue
+                val[2] = abs(values[axis][0]) + default_interval
+
+        # return x, y, z or no axis
+        def detect_axis(values, default_interval):
+            for index, val in enumerate(values):
+                # value smaller than interval
+                if abs(val[0]) < val[2]:
+                    continue
+                else:
+                    return index
+            return None
+
+        def max_val(values, axis):
+            if axis == None:
+                return
+            
+            if abs(values[axis][3]) < abs(values[axis][0]):
+                values[axis][3] = values[axis][0]
+
+        counter = 0
+        while True:
+            # poll accel values
+            acc(a, values)
+            # detect axis being shaken
+            axis = detect_axis(values, default_interval)
+            # recalculate interval for other axis
+            calculate_interval(axis, values, default_interval)
+            max_val(values, axis)
+            #max_set = max_val(values, axis)
+            #print(values[2])
+            #continue
+            #print(values[1])
+            #print(axis)
+            if axis == None:
+                #print(values)
+                continue
+            
+            #print(values[0])
+            try:
+                if values[axis][3] / values[axis][1] <= 0:
+                    print(axis)
+                    print(values)
+                    values[axis][3] = 0
+                    if values[axis][0] <= 0:
+                        self.audio.play(0)
+                    else:
+                        self.audio.play(1)
+                    time.sleep(0.01)
+                    continue
+            except ZeroDivisionError as err:
+                continue
+                
+            if values[axis][1] > 0 and values[axis][0] > 0:
+                continue
+            if values[axis][1] < 0 and values[axis][0] < 0:
+                continue
+        
     
     def _end(self):
         raise(NotImplementedError)
--- test/test-imu.py
+++ test/test-imu.py
@@ -1,35 +1,98 @@
 import gc
 from micropython import mem_info
 from machine import Pin, I2C
-from lib.mpu6050 import accel
+from mpu6050 import accel
+import time
 
-
-i = I2C(scl=Pin(5), sda=Pin(4), freq = 10000)
+i = I2C(scl=Pin(12), sda=Pin(13), freq = 100000)
 print(i)
 a = accel(i)
 mem_info()
 
-interval = 5000
+default_interval = 10000
 
 x = 0
 last_x = 0
 
-last_x = a.get_values()['AcX']
-counter=0
+# [[current val, last val, interval, max val], ...]
+values = [[0,0,default_interval, 0],[0,0,default_interval, 0],[0,0,default_interval, 0]]
+max_set = 0
+def acc(accel, values):
+    raw = accel.get_values()
+    axis = [0,0,0]
+    axis[0] = raw['AcX']
+    axis[1] = raw['AcY']
+    axis[2] = raw['AcZ'] - 19000
+    
+    
+    
+    for index, val in enumerate(values):
+        
+        #if abs(axis[index]) > 500:            
+            # copy current value to last value
+            val[1] = val[0]
+            # update current value with new value
+            val[0] = axis[index]
+        
+
+
+def calculate_interval(axis ,values, default_interval):
+    if axis == None:
+        for val in values:
+            val[2] = default_interval
+            #print("resetting interval for all")
+        return
+    
+    for index, val in enumerate(values):
+        if index == axis:
+            continue
+        val[2] = abs(values[axis][0]) + default_interval
+
+# return x, y, z or no axis
+def detect_axis(values, default_interval):
+    for index, val in enumerate(values):
+        # value smaller than interval
+        if abs(val[0]) < val[2]:
+            continue
+        else:
+            return index
+    return None
+
+def max_val(values, axis):
+    if axis == None:
+        return
+    
+    if abs(values[axis][3]) < abs(values[axis][0]):
+        values[axis][3] = values[axis][0]
+
+counter = 0
 while True:
-    counter +=1
-    if not counter % 50 :
-        mem_info()
-    x = a.get_values()['AcX']
-    if last_x > 0 and x > 0:
+    # poll accel values
+    acc(a, values)
+    # detect axis being shaken
+    axis = detect_axis(values, default_interval)
+    # recalculate interval for other axis
+    calculate_interval(axis, values, default_interval)
+    max_val(values, axis)
+    #max_set = max_val(values, axis)
+    #print(values[2])
+    #continue
+    #print(values[1])
+
+    if axis == None:
+        #print(values)
         continue
-    if last_x < 0 and x < 0:
+    
+    #print(values[0])
+    
+    if values[axis][3] / values[axis][1] <= 0:
+        print(axis)
+        print(values)
+        values[axis][3] = 0
+        time.sleep(0.01)
         continue
     
-    if x < interval and x > -interval:
+    if values[axis][1] > 0 and values[axis][0] > 0:
         continue
-    
-    print(f"last: {last_x}, curr: {x}")
-    last_x = x
-    
-    
+    if values[axis][1] < 0 and values[axis][0] < 0:
+        continue
